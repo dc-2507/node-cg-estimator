9fde8818a85e6bbb72f32e68e1195eb0498b2641
dc-2507 <dchaturvedi1_be16@thapar.edu>
2019-09-13 16:15:36 +0530

    Deploying to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var hbs = require('hbs');
 var app = express();
-
+const port = process.env.PORT || 3000;
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
@@ -10,6 +10,6 @@ app.get('/',(req,res)=>{
   res.render('home.hbs');
 });
 
-app.listen(3000,()=>{
-  console.log('Server is up and running on port 3000.')
+app.listen(port,()=>{
+  console.log(`Server is up and running on port ${port}.`)
 });
